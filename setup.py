fc3330a3304f5cbfe165f2f33a3c742c222dcb42
Safa Bayar <admin@example.com>
2019-03-23 18:19:14 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name = 'Split-File',
+	version = '1.0.1',
+	author = 'rection',
+	author_email = 'rectionxx@gmail.com',	
+	packages = ['Split-File'],
+	description = "File parted",
+	long_description = "it's parted files, websites, public files and remote server",
+	url = "https://gitlab.com/rection/Split-File"
+)
+
